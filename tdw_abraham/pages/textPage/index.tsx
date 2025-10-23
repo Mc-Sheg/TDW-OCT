3b8a70565530f8d48779db9c67747cf2acbd7cd4
mc-sheg <akinyeleabraham@ua.pt>
2025-10-23 19:15:14 +0100

    commit image

---
+++ tdw_abraham/pages/textPage/index.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import React from 'react'
+
+function TextPages() {
+  return (
+    <div>
+        <p>
+            <Image src="/11.png" alt="Alternative"  width={30} height={30}/>
+        </p>
+    </div>
+  )
+}
+
+export default TextPages
